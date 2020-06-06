55e55e05ab63aeebd83a8f36e3c8d54a1493468e
Beato Bongco <beatobongco@gmail.com>
2020-06-06 09:16:39 +0800

    Create API endpoint for bm25

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from search import TfidfSearch, BM25Search, load_corpus
 
 app = Flask(__name__)
@@ -9,15 +9,23 @@ bm25_search = BM25Search(corpus)
 
 
 @app.route("/")
-@app.route("/bm25")
+@app.route("/tfidf")
 def index():
+    query = request.args.get("query", "")
+    results = tfidf_search.search(query) if query else []
+    return render_template("search.html", query=query, results=results, prefix="TF-IDF")
+
+
+@app.route("/bm25")
+def bm25():
     query = request.args.get("query", "")
     results = bm25_search.search(query) if query else []
     return render_template("search.html", query=query, results=results, prefix="BM25")
 
 
-@app.route("/tfidf")
-def tfidf():
-    query = request.args.get("query", "")
-    results = tfidf_search.search(query) if query else []
-    return render_template("search.html", query=query, results=results, prefix="TF-IDF")
+@app.route("/api/bm25", methods=["POST"])
+def bm25_api():
+    print(request.get_json())
+    query = request.get_json().get("query")
+    results = bm25_search.search(query) if query else []
+    return jsonify(results)
